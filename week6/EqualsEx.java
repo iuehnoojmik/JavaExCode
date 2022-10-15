8680b9dd4b9c28b2d30b3d525d799b1152ce4812
Joonheui <iuehnoojmik@naver.com>
2022-10-15 19:46:09 +0900

    Update EqualsEx.java

--- week6/EqualsEx.java
+++ week6/EqualsEx.java
@@ -1,14 +1,14 @@
-//class Point {
-//	int x, y;
-//	public Point(int x, int y) {
-//		this.x = x; this.y = y;
-//	}
-//	public boolean equals(Object obj) {
-//		Point p = (Point)obj; 
-//		if(x == p.x && y == p.y) return true;
-//		else return false;
-//	}
-//}
+class Point {
+	int x, y;
+	public Point(int x, int y) {
+		this.x = x; this.y = y;
+	}
+	public boolean equals(Object obj) {
+		Point p = (Point)obj; 
+		if(x == p.x && y == p.y) return true;
+		else return false;
+	}
+}
 
 public class EqualsEx {
 	public static void main(String[] args) {
